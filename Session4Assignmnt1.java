7dda0d9b6f26fec928f414757bf1b9634a29104b
Pratik Ratnaparkhi <pratikr557@gmail.com>
2016-05-30 22:35:04 +0530

    Add files via upload

---
+++ Session4Assignmnt1.java
@@ -0,0 +1,24 @@
+
+public class Session4Assignmnt1 
+
+
+{
+		
+    public static void main(String[] args) {
+    
+    	
+    	//intializing a variable
+    	int var[] = new int[] {1,2,3,4,5,6,7};
+        
+    	
+    	//condition
+    	for(int i = var.length - 1; i >= 0 ; i--) 
+    	
+    	{
+        
+    		
+    		//printing the result
+    		System.out.println(var[i]);
+        }
+    }
+}
